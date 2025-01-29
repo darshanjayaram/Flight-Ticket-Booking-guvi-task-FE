172604880215af9ff51ad6ae383423a2a55af9ac
darshanjayaram <darshansj.darshu@gmail.com>
2025-01-29 20:00:56 +0530

    updated

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,2 +1,2 @@
-export const BACKENDURL = "https://avbsbackend.onrender.com";
+export const BACKENDURL = "https://flight-ticket-booking-guvi-task-be.onrender.com";
 // export const BACKENDURL = "http://localhost:5000";
